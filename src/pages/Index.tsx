e40a6b31a9b127fe83815ff5f4b6279bb7d9a7ae
Samuel Santos <88847756+SamSantos7@users.noreply.github.com>
2025-05-13 15:01:39 -0300

    4

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import Hero from '@/components/Hero';
 import DestinationSection from '@/components/DestinationSection';
 import FeaturedAccommodations from '@/components/FeaturedAccommodations';
+// Altere esta linha:
 import WhyChooseUs from '@/components/WhyChooseUs';
+// Para esta:
+import WhyChooseUs from '@/components/layout/WhyChooseUs';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
