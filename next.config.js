70184efcbb62f2cb9b907e6407821994e3e85a58
Samuel Santos <88847756+SamSantos7@users.noreply.github.com>
2025-05-14 12:41:18 -0300

    10.9

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   // Certifique-se de que a configuração de páginas está correta
   // Se você estiver usando o diretório src/pages
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  
+  webpack: (config, { isServer }) => {
+    // Aumenta o limite de aviso de tamanho do chunk para 1MB (ou o valor que preferir)
+    config.performance = {
+      ...config.performance,
+      maxEntrypointSize: 1000000, // 1MB
+      maxAssetSize: 1000000 // 1MB
+    };
+    
+    return config;
+  },
 }
 
 module.exports = nextConfig
